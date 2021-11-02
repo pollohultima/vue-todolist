c2f557bd7e78a5050b0da8bf2a8d060203d2f5df
pollohultima <matteo.polonio@gmail.com>
2021-11-02 12:18:58 +0100

    program works
    starting css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,35 @@
 const app = new Vue({
-
     el: '#root',
 
     data: {
+        tasks: [
+            'Fare la spesa',
+            'Andare in palestra',
+            'Imparare VueJs'
+        ],
 
+        newTask: '',
 
+        error: false,
+
+        imgUrl: 'https://www.boolean.careers/images/misc/logo.png'
     },
 
     methods: {
 
+        deleteTask(i) {
+            this.tasks.splice(i, 1)
+        },
 
+        addTask() {
+            if (this.newTask != '' && this.newTask.length > 5) {
+                this.tasks.push(this.newTask)
+                this.error = false
+            } else {
+                this.error = true
+            }
 
-
+            this.newTask = ''
+        }
     }
-
-
-
-
-
-
-
-
 })
